7a4c68d425b504278b95da144fb1e92a1623082f
Prodeon <prodeon21@gmail.com>
2025-11-19 09:10:26 +0300

    add class Category

---
+++ src/category.py
@@ -0,0 +1,14 @@
+class Category:
+    name : str
+    description : str
+    products : list
+
+    count_of_catogories = 0
+    count_of_products = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.count_of_catogories += 1
+        Category.count_of_products += len(self.products)
