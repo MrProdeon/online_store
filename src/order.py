b97f5f3e23ace4566cb65728b2cba1bd602b21e5
Prodeon <prodeon21@gmail.com>
2025-12-06 15:42:03 +0300

    Описан новый класс Order, который наследуется от абстрактного класса OrderCategory. Класс имеет информацию об объекте продукта,может рассчитать стоимость заказа, а так же прописано строковое представление заказа

---
+++ src/order.py
@@ -0,0 +1,25 @@
+from src.category import OrderCategory
+from src.product import Product
+
+class Order(OrderCategory):
+    """Класс заказа, может содержать только один заказанный товар.
+    Наследуется от абстрактного класса OrderCategory, который так же является родителем класса Category,
+    берет из него описание и обязательный метод __str__
+
+    Имеет метод для рассчитывания общей стоимости заказа, исходя из цены и кол-ва
+
+    Для строкового представления вернет информация о составе заказа, кол-во товаров и общей стоимости
+    """
+
+    def __init__(self, description, product, quantity):
+        super().__init__(description)
+        self.product = product
+        self.price = product.price
+        self.quantity = quantity
+
+    @property
+    def total_price(self):
+        return self.price * self.quantity
+
+    def __str__(self):
+        return f"Состав заказа : {self.product.name}, в количестве {self.quantity}шт; общая стоимость {self.total_price}"
